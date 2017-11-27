10842c4bb238d64917c29c3252d8ec2e701b5a4f
Aurélien Loyer <aur.loy@gmail.com>
2017-11-27 10:08:34 +0100

    feat(slide): base for trinket code

---
+++ OSX_open_slide_trinket/OSX_open_slide_trinket.ino
@@ -0,0 +1,11 @@
+#include <TrinketKeyboard.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  TrinketKeyboard.begin();
+  TrinketKeyboard.print("Hello World!");
+
+}
+
+void loop() {
+}
