7a6ce18c7641a6bdcc80a4bc77788b3a14bad26f
Aurélien Loyer <aur.loy@gmail.com>
2018-09-26 11:15:55 -0400

    feat: trinket print C0D1NG_TH3_W0RLD 🌈

---
+++ C0D1NG_TH3_W0RLD_trinket/C0D1NG_TH3_W0RLD_trinket.ino
@@ -0,0 +1,10 @@
+#include <TrinketKeyboard.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  TrinketKeyboard.begin();
+  TrinketKeyboard.print(" C0D1NG_TH3_W0RLD ");
+}
+
+void loop() {
+} 
